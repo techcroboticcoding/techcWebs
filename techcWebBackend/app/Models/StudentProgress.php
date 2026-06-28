b24db6ff04103beaa0039385578a805e2355142e
Adam Rizki <107348054+adamrzkys2@users.noreply.github.com>
2026-06-28 23:47:44 +0700

    Update StudentProgress.php

--- techcWebBackend/app/Models/StudentProgress.php
+++ techcWebBackend/app/Models/StudentProgress.php
@@ -1,13 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class StudentProgress extends Model
 {
-    protected $fillable = [
+    protected $table = 'student_progress';
 
+    protected $fillable = [
         'student_id',
         'progress',
         'level',
         'status',
-        'teacher_note'
-
+        'teacher_note',
     ];
 
     public function student()
